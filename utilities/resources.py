bce53acd246cdb300f46c5206ee9f28b311a37a3
Igor Ferro <igor.ferro.parreira@gmail.com>
2022-11-18 15:42:28 -0300

    Behave bdd project

--- utilities/resources.py
+++ utilities/resources.py
@@ -1,7 +1,7 @@
 class ApiResources:
-    apikey = 'd6fa171a71b818ffd0455d1c59b8c554'
-    ts = '1668507926'
-    hash = '0605d67484f892bc6f57b83297be12f5'
+    apikey = ''
+    ts = ''
+    hash = ''
 
     getStories = f'/stories?limit=5&offset=100&apikey={apikey}&ts={ts}&hash={hash}'
     getCharatersOne = f'/characters/1011198?apikey={apikey}&ts={ts}&hash={hash}'
